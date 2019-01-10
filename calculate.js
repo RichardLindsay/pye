411ce72c42f5978c69fbb31f057f6df61abbef10
Richard Lindsay <richard@tictocfamily.com>
2019-01-10 08:24:04 +0000

    Add calculator

--- calculate.js
+++ calculate.js
@@ -19,12 +19,24 @@ window.updateBidsDetail = function() {
 };
 
 window.updateWatchList = function() {
-  console.log(document.querySelectorAll('td[width="15%"] font'));
   document.querySelectorAll('font[color="#eE2C54"]').forEach(function(e) {
     window.addElement('p', window.withBuyersPremium(window.withVat(window.currentBid(e))), e);
     window.addElement('p', window.withVat(window.currentBid(e)), e);
   });
 };
+
+window.createCalculator = function() {
+  let calculator = document.createElement('div')
+  calculator.style.cssText = "display: flex; flex-direction: column; padding: 20px; align-items: center; justify-content: space-between; position: fixed; top: 0; right: 0; min-width: 200px; min-height: 200px; background-color: white;"
+  calculator.innerHTML = '<form><label style="margin-right: 5px">£</label><input type="text" class="potentialBid"></form><h2 style="color: green">£0</h2><h3 style="color: orange">£0</h3>'
+  document.body.prepend(calculator)
+
+  document.querySelector('.potentialBid').addEventListener('keyup', function() {
+    calculator.querySelector('h3').innerText = `£${window.withVat(document.querySelector('.potentialBid').value).toFixed(2)}`;
+    calculator.querySelector('h2').innerText = `£${window.withBuyersPremium(window.withVat(document.querySelector('.potentialBid').value)).toFixed(2)}`;
+
+  });
+}
 
 // Helper methods
 
@@ -49,3 +61,4 @@ window.withBuyersPremium = function(value) {
 window.updateBidsList();
 window.updateBidsDetail();
 window.updateWatchList();
+window.createCalculator();
--- highlight.js
+++ highlight.js
@@ -4,25 +4,13 @@ window.removeAuctions = function() {
   let count = 0;
 
   document.querySelectorAll('a[href*="auction_details"]').forEach(function(e) {
-    if (!e.innerHTML.toLowerCase().includes('edin')) {
-      e.closest('tr').style.display = 'none';
+    if (!e.innerHTML.toLowerCase().includes('near edin')) {
+      e.closest('tr').style.opacity = 0.5;
     }
     else {
       count++;
     }
   });
-
-  if (count === 0) {
-    window.addElement('h2', 'No auctions found', document.querySelector('table[cellpadding="2"] tbody'));
-  }
-};
-
-// Helper methods
-
-window.addElement = function(type, value, parent) {
-  let el = document.createElement(type);
-  el.innerHTML = value;
-  parent.parentNode.insertBefore(el, parent);
 };
 
 window.removeAuctions();
